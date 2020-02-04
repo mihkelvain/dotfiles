7657a10fd412493fad63c6b1471246c2235055ea
Mihkel Vain <mihkel.vain@tallink.ee>
2020-02-04 14:02:24 +0200

    Add time to left while on battery to swaybar

--- sway/.config/sway/status.sh
+++ sway/.config/sway/status.sh
@@ -1,6 +1,7 @@
 date_formatted=$(date "+%a / %H:%M / %d-%m-%y")
 audio_volume=$(amixer -M get Master | grep "Front Left:" | awk '{print $6=="[off]" ? $5" 🔇":  $5" 🔉"}' | tr -d [])
-battery_status=$(cat /sys/class/power_supply/BAT0/status)
+battery_info=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "state|percentage" | awk '{print $2}')
+time_to_empty=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "time to empty" | awk '{print $4,$5}')
 # Emojis and characters for the status bar
-# 💎 💻 💡 🔌 ⚡ 📁 \|
-echo $audio_volume $battery_status 🔋 $date_formatted
+# 💎 💻 💡 🔌 ⚡ 📁
+echo $audio_volume $battery_info [Time left: $time_to_empty] 🔋  $date_formatted
